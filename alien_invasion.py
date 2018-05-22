523c85647eca5e116a31e2b19a85954b5e0813d5
toky <979564561@qq.com>
2018-05-22 23:57:30 +0800

    5.22 up to 13.3

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import  pygame
 from pygame.sprite import Group
 from settings import Settings
 from ship import Ship
+from alien import Alien
 import game_functions as gf
 
 def run_game():
@@ -20,13 +21,16 @@ def run_game():
     ship = Ship(ai_settings,screen)
     #创建一个用于存储子弹的编组
     bullets = Group()
-
+    # 创建一个外星人
+    alien = Alien(ai_settings, screen)
 
     #开始游戏的主循环
     while True:
 
         gf.check_events(ai_settings,screen,ship,bullets)
         ship.update()
-        bullets.update()
-        gf.update_screen(ai_settings,screen,ship,bullets)
+        gf.update_bullets(bullets)
+        #print(len(bullets))
+
+        gf.update_screen(ai_settings,screen,ship,alien,bullets)
 run_game()
--- game_functions.py
+++ game_functions.py
@@ -8,9 +8,11 @@ def check_keydown_events(event,ai_settings,screen,ship,bullets):
     elif event.key == pygame.K_LEFT:
         ship.moving_left = True
     elif event.key == pygame.K_SPACE:
-        #创建一颗子弹，并将器编组到bullets中
-        new_bullet = Bullet(ai_settings,screen,ship)
-        bullets.add(new_bullet)
+        fire_bullet(ai_settings, screen, ship, bullets)
+    elif event.key == pygame.K_q:
+        sys.exit()
+
+
 
 def check_keyup_events(event,ship):
     """响应松开"""
@@ -29,13 +31,30 @@ def check_events(ai_settings,screen,ship,bullets):
         elif event.type == pygame.KEYUP:
             check_keyup_events(event,ship)
 
-def update_screen(ai_settings,screen,ship,bullets):
+def update_screen(ai_settings,screen,ship,alien,bullets):
     """更新屏幕上的图像，并切换到新屏幕"""
     screen.fill(ai_settings.bg_color)
     #飞船和外星人后面重绘所有子弹
     for bullet in bullets.sprites():
         bullet.draw_bullet()
     ship.blitme()
+    alien.blitme()
 
     #让最近绘制的屏幕可见
     pygame.display.flip()
+
+def update_bullets(bullets):
+    """更新子弹的位置，并删除已消失的子弹"""
+    #更新子弹的位置
+    bullets.update()
+    #删除已消失的子弹
+    for bullet in bullets.copy():
+        if bullet.rect.bottom <= 0:
+            bullets.remove(bullet)
+
+def fire_bullet(ai_settings, screen, ship, bullets):
+    """"如果还没有到达限制， 就先发射一颗子弹"""
+    #创建新子弹，并将器编组到bullets中
+    if len(bullets) < ai_settings.bullet_allowed:
+        new_bullet = Bullet(ai_settings,screen,ship)
+        bullets.add(new_bullet)
